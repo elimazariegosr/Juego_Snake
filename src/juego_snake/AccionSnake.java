1e7e3d33e985e961e44548a225276f8a36cf1af8
eli mazariegos <elimazariegosr@gmail.com>
2017-12-08 19:32:53 -0400

    Commit acciones de Snake

--- src/juego_snake/AccionSnake.java
+++ src/juego_snake/AccionSnake.java
@@ -9,6 +9,20 @@ package juego_snake;
  *
  * @author Mazariegos
  */
-public class AccionSnake {
+public class AccionSnake extends Posicion {
     
+    //Constructor AccionSnake que recibe a la culebrita a donde moverse
+    public AccionSnake(int x, int y, char figura){
+        setX(x);
+        setY(y);
+        setFigura(figura);
+    }
+    public void arriba(){
+    }
+    public void abajo(){
+    }
+    public void izquierda(){
+    }
+    public void derecha(){
+    }
 }
