58b7be42da6be7ea684f498d40ccb57cefedcaca
eli mazariegos <elimazariegosr@gmail.com>
2017-12-09 17:30:30 -0400

    tablero del juego mostrado

--- src/juego_snake/Juego_Snake.java
+++ src/juego_snake/Juego_Snake.java
@@ -13,15 +13,24 @@ public class Juego_Snake {
         
         
     public static void main(String[] args) {
-        int base=70;
+        int ancho=70;
         int altura=35;
 
         /* MenuOpcion menu = new MenuOpcion();
          menu.opciones();*/
         //probando tablero
-         Tablero tablero = new Tablero(base,altura);
+         Tablero tablero = new Tablero(ancho,altura);
          tablero.tabPosicion();
          //mostrar asterisco como muro
+        MuroTablero muro = new MuroTablero('#');
+        //filas
+        muro.mostrarFilaMuro(tablero, muro,0);
+        muro.mostrarFilaMuro(tablero, muro, tablero.getAltura() -1);
+        //Columnas
+        muro.mostrarColumnaMuro(tablero, muro, 0);
+        muro.mostrarColumnaMuro(tablero, muro, tablero.getAncho() -1);
+         
+       tablero.mostrarTablero();
     }
     
 }
